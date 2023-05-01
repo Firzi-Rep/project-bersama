c8497dd67c7ba10617006c5440e6965e006f4617
Dimas Indra Jaya <dimaszxc101@gmail.com>
2023-05-01 15:50:38 +0700

    masih salah

--- script/script.js
+++ script/script.js
@@ -1,3 +1,11 @@
 function formulir() {
   document.getElementById("formulir-box").classList.toggle("daftar-isi");
 }
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const fd = new FormData(form);
+  console.log(fd);
+});
